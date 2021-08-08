aa6f6ccf902f0814d538a5f358516b4ee3ce3161
Christoffer G. Thomsen <chris@cgt.name>
2021-08-08 16:08:09 +0200

    It compiles

--- main.cpp
+++ main.cpp
@@ -19,69 +19,74 @@ class Game {
 	int x = 0;
 	int y = 0;
 
-	void setup() {
-		initscr();
-		cbreak();
-		noecho();
-		curs_set(false);
-		nodelay(stdscr, true);
-		keypad(stdscr, true);
-	}
+	void setup();
 
 	public:
-		void run() {
-			setup();
-			int max_y = 0;
-			int max_x = 0;
-			getmaxyx(stdscr, max_y, max_x);
-
-			bool zoom = false;
-			std::vector<Position> trail;
-			while (1) {
-				if (quit) {
-					break;
-				}
-				int ch = getch();
-				int steps = zoom ? 3 : 1;
-
-				if (ch != ERR) {
-					Position p{y, x};
-					trail.push_back(p);
-					if (trail.size() > 10) {
-						trail.erase(trail.begin());
-					}
-				}
-
-				switch (ch) {
-					case KEY_LEFT:
-						x -= steps;
-						break;
-					case KEY_RIGHT:
-						x += steps;
-						break;
-					case KEY_UP:
-						y -= steps;
-						break;
-					case KEY_DOWN:
-						y += steps;
-						break;
-					case 'z':
-						zoom = !zoom;
-						break;
-				}
-				erase();
-				border(0, 0, 0, 0, 0, 0, 0, 0);
-				for (auto p : trail) {
-					mvaddch(p.y, p.x, ACS_DIAMOND);
-				}
-				mvprintw(y, x, "o");
-				refresh();
-				usleep(5000);
-			}
-
-			endwin();
-		}
+	void run();
 };
+
+void Game::setup() {
+	initscr();
+	cbreak();
+	noecho();
+	curs_set(false);
+	nodelay(stdscr, true);
+	keypad(stdscr, true);
+}
+
+
+void Game::run() {
+	setup();
+	int max_y = 0;
+	int max_x = 0;
+	getmaxyx(stdscr, max_y, max_x);
+
+	bool zoom = false;
+	std::vector<Position> trail;
+	while (1) {
+		if (quit) {
+			break;
+		}
+		int ch = getch();
+		int steps = zoom ? 3 : 1;
+
+		if (ch != ERR) {
+			Position p{y, x};
+			trail.push_back(p);
+			if (trail.size() > 10) {
+				trail.erase(trail.begin());
+			}
+		}
+
+		switch (ch) {
+			case KEY_LEFT:
+				x -= steps;
+				break;
+			case KEY_RIGHT:
+				x += steps;
+				break;
+			case KEY_UP:
+				y -= steps;
+				break;
+			case KEY_DOWN:
+				y += steps;
+				break;
+			case 'z':
+				zoom = !zoom;
+				break;
+		}
+		erase();
+		border(0, 0, 0, 0, 0, 0, 0, 0);
+		for (auto p : trail) {
+			mvaddch(p.y, p.x, ACS_DIAMOND);
+		}
+		mvprintw(y, x, "o");
+		refresh();
+		usleep(5000);
+	}
+
+	endwin();
+}
 
 int main(int argc, char **argv) {
 	signal(SIGINT, handleInterrupt);
